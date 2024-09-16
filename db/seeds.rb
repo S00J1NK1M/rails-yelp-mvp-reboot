a7d5c6f16e3b3af30cc6d454ffdc1cca47d8f9dd
Soojin Kim <contact.soojin.kim@gmail.com>
2024-09-16 16:24:10 +0200

    challenge done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+facil = Restaurant.new(name: 'Facil', address: 'lwstr.1 Berlin', phone_number: '012345', category: 'french')
+facil.save!
+
+luna = Restaurant.new(name: 'Luna', address: 'lwstr.2 Berlin', phone_number: '0123456', category: 'italian')
+luna.save!
+
+enzo = Restaurant.new(name: 'Enzo', address: 'lwstr.3 Berlin', phone_number: '01234567', category: 'japanese')
+enzo.save!
+
+crackers = Restaurant.new(name: 'Crackers', address: 'lwstr.4 Berlin', phone_number: '012345678', category: 'belgian')
+crackers.save!
+
+mexicana = Restaurant.new(name: 'Mexicana', address: 'lwstr.5 Berlin', phone_number: '54321', category: 'chinese')
+mexicana.save!
